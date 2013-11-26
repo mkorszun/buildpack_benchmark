441d0081452292a52f3d0627ca8c54d3200b8da3
Mateusz Korszun <mkorszun@gmail.com>
2013-11-26 16:48:41 +0100

    First version

---
+++ benchmark.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PREFIX="[BENCHMARK]"
+ERROR="[ERROR]"
+BUILDPACK_URL=$1
+APP_NAME="benchmark$(date | md5 | fold -w20 | head -n1)"
+LOG_FILE="./output.log"
+
+echo "$PREFIX Creating test application $APP_NAME"
+cctrlapp $APP_NAME create custom --buildpack=$BUILDPACK_URL 1>&2 2>$LOG_FILE
+
+if [ $? -ne 0 ]; then
+    echo "$ERROR App creation failed. Check $LOG_FILE";
+    exit 1;
+fi
+
+echo "$PREFIX Pushing test application $APP_NAME"
+cctrlapp $APP_NAME/default push 1>&2 2>$LOG_FILE
+
+if [ $? -ne 0 ]; then
+    echo "$ERROR Push failed. Check $LOG_FILE";
+    exit 1;
+else
+    echo "$PREFIX Build time: $(grep 'Total time' $LOG_FILE | awk '{print $6}')"
+fi
+
