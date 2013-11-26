36143d543de9ed1a71f4fa2b0c03b5189c1d9c1d
Mateusz Korszun <mkorszun@gmail.com>
2013-11-26 17:29:32 +0100

    Cleanup after test

--- benchmark.sh
+++ benchmark.sh
@@ -19,8 +19,11 @@ cctrlapp $APP_NAME/default push 1>&2 2>$LOG_FILE
 
 if [ $? -ne 0 ]; then
     echo "$ERROR Push failed. Check $LOG_FILE";
+    cctrlapp $APP_NAME delete -f
     exit 1;
 else
+    cctrlapp $APP_NAME/default undeploy -f
+    cctrlapp $APP_NAME delete -f
     echo "$PREFIX Build time: $(grep 'Total time' $LOG_FILE | awk '{print $6}')"
 fi
 
