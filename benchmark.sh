e83bedb44f3edd578b6fb8ebb236a9a7fe6a6f1a
Mateusz Korszun <mkorszun@gmail.com>
2013-11-27 13:58:11 +0100

    Cleanup after interruption

--- benchmark.sh
+++ benchmark.sh
@@ -14,6 +14,14 @@ else
     fi
 fi
 
+function cleanup() {
+    echo "$PREFIX Cleaning up..."
+    cctrlapp $APP_NAME/default undeploy -f 1>&2 2>$LOG_FILE
+    cctrlapp $APP_NAME delete -f 1>&2 2>$LOG_FILE
+    echo "$PREFIX Done!"
+    exit 0
+}
+
 PREFIX="[BENCHMARK]"
 ERROR="[ERROR]"
 BUILDPACK_URL=$1
@@ -22,6 +30,7 @@ LOG_FILE="./output.log"
 
 echo "$PREFIX Creating test application $APP_NAME"
 cctrlapp $APP_NAME create custom --buildpack=$BUILDPACK_URL 1>&2 2>$LOG_FILE
+trap cleanup INT
 
 if [ $? -ne 0 ]; then
     echo "$ERROR App creation failed. Check $LOG_FILE";
@@ -33,12 +42,8 @@ cctrlapp $APP_NAME/default push 1>&2 2>$LOG_FILE
 
 if [ $? -ne 0 ]; then
     echo "$ERROR Push failed. Check $LOG_FILE";
-    cctrlapp $APP_NAME delete -f
-    exit 1;
 else
     echo "$PREFIX Build time: $(grep 'Total time' $LOG_FILE | awk '{print $6}')"
-    echo "Cleaning up..."
-    cctrlapp $APP_NAME/default undeploy -f
-    cctrlapp $APP_NAME delete -f
-    echo "Done!"
 fi
+
+cleanup
